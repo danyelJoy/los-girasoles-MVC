7f245ead3bdd51e0d74813b1dadfde42a11465f7
danyelJoy <85145389+danyelJoy@users.noreply.github.com>
2021-10-20 00:26:30 -0500

    proyecto los girasoles MVC

---
+++ app.js
@@ -0,0 +1,139 @@
+require('dotenv').config(); // CONFIGURACION EN HERUKI
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const passport = require('./config/passport');
+const session = require('express-session');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var usuariosRouter = require('./routes/usuarios')
+var tokenRouter = require('./routes/token');
+var productosRouter = require('./routes/productos');
+var bicicletasRouter = require('./routes/bicicletas');
+
+const store = new session.MemoryStore;
+
+//var usuariosAPIRouter = require('./routes/api/usuarios');
+
+
+var app = express();
+
+app.use(session({
+  cookie: {maxAge: 240 * 60 * 60 * 1000},
+  store: store,
+  saveUninitialized: true,
+  resave: 'true',
+  secret: 'los_girasoles_ !!!!"!"!""!123'
+}));
+
+var mongoose = require('mongoose');
+
+//mongodb+srv://admin:xMSEogg8tkunGhCe@losgirasoles.eavp4.mongodb.net/test
+
+const usuario = require('./models/usuario');
+//var mongoDB = 'mongodb://localhost/losgirasoles';
+//var mongoDB = 'mongodb+srv://admin:xMSEogg8tkunGhCe@losgirasoles.eavp4.mongodb.net/test';
+
+var mongoDB = process.env.MONGO_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('err', console.error.bind(console, 'MongoDB connection error'));
+
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/login', function(req,res){
+  res.render('session/login');
+});
+
+app.post('/login', function(req, res, next){
+  passport.authenticate('local', function(err, usuario,info){
+    if(err) return next(err);
+    if(!usuario) return res.render('session/login', {info});
+    req.login(usuario, function(err){
+      if (err) return next(err);
+      return res.redirect('/productos');
+    });
+  })(req, res, next);  
+});
+
+app.get('/logout', function(req, res){
+  req.logOut();
+  res.redirect('/');
+});
+
+app.get('forgotPassord', function(req, res){
+  res.render('session/forgotPassword');
+
+});
+
+app.post('/forgotPassword', function(req, res){
+  Usuario.findOne({email: req.body.email}, function (err, usuario){
+    if(!usuario) return res.render('session/forgotPassword', {info: {message: "No existe el usuario"}})
+
+    usuario.resetPassword(function(err){
+      if(err) return next(err);
+      console.log('session/forgotPasswordMessage');      
+    });
+    res.render('session/forgotPasswordMessage')
+  });
+});
+
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/usuarios', usuariosRouter);
+app.use('token',tokenRouter);
+app.use('/bicicletas', bicicletasRouter);
+app.use('/productos',loggedIn, productosRouter);
+//app.use('/api/usuarios', usuariosAPIRouter)
+
+app.use('privacy_policy', function(req, res){
+  res.sendFile('public/privacy_policy.html');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+function loggedIn (req, res, next){
+  if(req.user){
+     next();
+  } else {
+    console.log('user sin loguearse');
+    res.redirect('/login');
+  }
+};
+
+module.exports = app;
