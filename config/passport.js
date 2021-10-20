d1d09dafedc124932400c069409ddc4f070d487b
danyelJoy <85145389+danyelJoy@users.noreply.github.com>
2021-10-20 00:36:14 -0500

    Add files via upload

---
+++ config/passport.js
@@ -0,0 +1,56 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+const Usuario = require('../models/usuario');
+//const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use( new LocalStrategy(
+    function(email, password, done){
+        Usuario.findOne({email: email}, function(err, usuario){
+            if(err) return done(err);
+            if(!usuario) return done(null, false, {message: 'Email no existe o incorrecto'})
+            if(!usuario.validPassword(password)) return done(null, false, {message: 'Password correcto'})
+
+            return done (null, usuario);
+        });
+    }
+
+));
+
+passport.serializeUser(function(user, cb){
+    cb(null, user.id)
+});
+
+passport.deserializeUser(function(id, cb){
+    Usuario.findById(id, function(err, usuario){
+        cb(err, usuario);
+    });
+});
+
+/* 
+passport.use(new GoogleStrategy ({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.HOST + "/auth/google/callback"
+},
+    function(accessToken, refreshToken, profile, cb){
+        console.log(profile);
+
+        Usuario.findOneOrCreateByGoogle(profile, function(err, user){
+            return cb(err, usuer);
+
+        });
+    })
+
+);
+
+passport.serializeUser(function(id, cb){
+    cb(null, user.id)
+});
+
+passport.deserializeUser(function(id, cb){
+    Usuario.findById(id, function(err, usuario){
+        cb(err, usuario);
+    });
+});
+ */
+module.exports = passport;
